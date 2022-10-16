85c1fd11108a1a66997c89e9dc09f65e5a2f1c97
Hiruni Piumika <piumikahiruni22@gmail.com>
2022-10-16 18:16:50 +0530

    order form edit

--- assignment/js/SPA/controller/orderController.js
+++ assignment/js/SPA/controller/orderController.js
@@ -1,7 +1,7 @@
 function loadCustomeCode(){
     $("#cmbCusCode").empty()
     for (let customer of customerArray){
-        $("#cmbCusCode").append(`<option>${customer.CusCode}</option>`)
+        $("#cmbCusCode").append(`<option value="${customer.CusCode}">${customer.CusCode}</option>`)
     }
 }
 
@@ -35,12 +35,31 @@ $('#cmbItemCode').click(function(){
     }
 })
 
+$("#orderId").val(genarateOrderId())
 //SAVE ORDER//
+function genarateOrderId(){
+    if (orderArray.length > 0) {
+        let id = orderArray[orderArray.length - 1].orderId;
+        let [pre, frag] = id.split("-");
+        let num = parseInt(frag) + 1;
+        let count = num.toString().length;
+        if (count == 1) {
+            return pre + "-00" + num;
+        } else if (count == 2) {
+            return pre + "-0" + num;
+        } else {
+            return pre + "-" + num;
+        }
+    } else {
+        return "O-001";
+    }
+}
 function saveOrder(){
     let orderId=$('#orderID').val();
     let customCode= $('#cmbCusCode').val();
     let date=$('#dateI').val()
     let itemsC=cartArray
+    let total = $("#subTot").val()
     // let cusIds;
     // let itemCodes;
     // let itemNames;
@@ -61,9 +80,11 @@ function saveOrder(){
         orderId,
         customCode,
         itemsC,
-        date
+        date,
+        total
     }
     orderArray.push(orderObj)
+    $("#orderId").val(genarateOrderId())
     // console.log('order='+orderArray)
     // console.log('price in= '+itemPrice)
 }
@@ -76,28 +97,32 @@ $('#placeOrderBtn').click(function(){
     console.log("this runs")
     clear()
     saveOrder()
-    $('#orderId').val(calculateNextId())
+    //$('#orderId').val(calculateNextId())
     alert('order has been saved!')
 
 
 })
+
 //find order id
 $('#orderId').on('keyup',function(event){
     if(event.which==13){
 
-        let order = orderArray.find((order) => {
-            return order.orderId ==  $('#orderId').val()
+        let order = orderArray.find((orderO) => {
+            return orderO.orderId ==  $('#orderId').val()
         })
         console.log(order)
         // $('#order-tabelbody').empty();
         order.itemsC.forEach(i => {
-                var TbaleRow=`<tr ><td>${i.customCode}</td><td>${i.itemCode1}</td><td>${i.itemName1}</td><td>${i.itemDes1}</td><td>${i.itemPrice1}</td><td>${i.itemsqty}</td><td>${i.Total}</td><td>${'<button class="btn btn-outline-danger" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" id="btnRemove">remove</button>'}</td></tr>`
+                var TbaleRow=`<tr ><td>${i.itemCode1}</td><td>${i.itemName1}</td><td>${i.itemDes1}</td><td>${i.itemPrice1}</td><td>${i.itemsqty}</td><td>${i.Total}</td><td>${'<button class="btn btn-outline-danger" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;" id="btnRemove">remove</button>'}</td></tr>`
                 $('#orderTBody').append(TbaleRow)
             }
         )
-        subtot(order.itemsC)
+
+        $("#subTot").val(order.total)
+        $("#cmbCusCode").val(order.customCode)
+
     }
-})
+}).va
 //
 // // subtotal
 // function subtot(array){
@@ -115,7 +140,7 @@ $('#orderId').on('keyup',function(event){
 //         let id = orderArray[orderArray.length - 1].orderId;
 //         let [pre, frag] = id.split("-");
 //         let num = parseInt(frag) + 1;
-//         let count = num.toString().length;
+//         let count = num.ltoString().length;
 //         if (count == 1) {
 //             return pre + "-00" + num;
 //         } else if (count == 2) {
